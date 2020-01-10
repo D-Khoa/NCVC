f56a075ef1b28e70cd2ce306ba1e8096d2968f14
DangKhoa <dk.wofts@gmail.com>
2020-01-10 12:56:14 +0700

    update tracy car

--- Tracy CAR v2.0/MovieDB/Form/Input.cs
+++ Tracy CAR v2.0/MovieDB/Form/Input.cs
@@ -1093,7 +1093,10 @@ namespace Tracy
 
             if (batchOld != string.Empty)
             {
-                dateOld = DateTime.ParseExact(VBStrings.Mid(batchOld, 10, 6), "yyMMdd", CultureInfo.InvariantCulture);
+                string[] temp = batchOld.Split('#');
+                string datetemp = temp[temp.Length - 2];
+                //dateOld = DateTime.ParseExact(VBStrings.Mid(batchOld, 10, 6), "yyMMdd", CultureInfo.InvariantCulture);
+                dateOld = DateTime.ParseExact(datetemp, "yyMMdd", CultureInfo.InvariantCulture);
                 numberOld = long.Parse(VBStrings.Right(batchOld, 4));
             }
 
